bea46551613761aef470250fcd2e172276136996
cisaacap <cisaacap@gmail.com>
2026-09-21 16:44:10 -0600

    rechanges dtos updates

--- src/main/java/com/cisaacap/tourney/dto/request/jugadores/JugadorRequest.java
+++ src/main/java/com/cisaacap/tourney/dto/request/jugadores/JugadorRequest.java
@@ -5,18 +5,25 @@ public class JugadorRequest {
     private String nombre;
     private String apellido;
     private int edad;
-    private int idEquipo;
     private int idUsuario;
+    private int idEquipo;
 
     public JugadorRequest() {
     }
 
-    public JugadorRequest(String nombre, String apellido, int edad, int idEquipo, int idUsuario) {
+    public JugadorRequest(String nombre, String apellido, int edad, int idUsuario) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
-        this.idEquipo = idEquipo;
         this.idUsuario = idUsuario;
+    }
+
+    public JugadorRequest(String nombre, String apellido, int edad, int idUsuario, int idEquipo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.idUsuario = idUsuario;
+        this.idEquipo = idEquipo;
     }
 
     public String getNombre() {
@@ -43,19 +50,19 @@ public class JugadorRequest {
         this.edad = edad;
     }
 
-    public int getIdEquipo() {
-        return idEquipo;
-    }
-
-    public void setIdEquipo(int idEquipo) {
-        this.idEquipo = idEquipo;
-    }
-
     public int getIdUsuario() {
         return idUsuario;
     }
 
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
     }
 }
